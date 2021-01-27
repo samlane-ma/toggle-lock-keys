4c1258a664ed3094cd8fbe6f6d90da51776164bd
Sam Lane <67085765+samlane-ma@users.noreply.github.com>
2021-01-27 16:18:40 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #/usr/bin/env bash
 
-mkdir /usr/lib/budgie-desktop/plugins/toggle-lock-keys
-cp *.py /usr/lib/budgie-desktop/plugins/toggle-lock-keys
-cp *.plugin /usr/lib/budgie-desktop/plugins/toggle-lock-keys
+sudo mkdir /usr/lib/budgie-desktop/plugins/toggle-lock-keys
+sudo cp *.py /usr/lib/budgie-desktop/plugins/toggle-lock-keys
+sudo cp *.plugin /usr/lib/budgie-desktop/plugins/toggle-lock-keys
